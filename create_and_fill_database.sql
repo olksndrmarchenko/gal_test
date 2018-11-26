60b6e517eaf0d727aeddb97624cdb3685a2c66d6
olksndr <rskibitskiy@condati.com>
2018-11-26 17:56:56 +0200

    additional improvements

--- create_and_fill_database.sql
+++ create_and_fill_database.sql
@@ -1,7 +1,7 @@
 create table transactions (
 WEEK_END_DATE date,
-STORE_NUM INT,
-UPC INT,
+STORE_NUM BIGINT,
+UPC BIGINT,
 UNITS INT,
 VISITS INT,
 HHS INT,
@@ -15,6 +15,18 @@ TPR_ONLY INT);
 
 COPY transactions(WEEK_END_DATE,STORE_NUM,UPC,UNITS,VISITS,HHS,SPEND,PRICE,BASE_PRICE,FEATURE,DISPLAY,TPR_ONLY) from '/home/olksndr/project/transactions.csv' DELIMITER ',' CSV HEADER;
 
+create table products (U
+PC BIGINT, 
+DESCRIPTION TEXT, 
+MANUFACTURER TEXT, 
+CATEGORY TEXT, 
+SUB_CATEGORY TEXT,
+PRODUCT_SIZE TEXT);
+
+
+COPY transactions(UPC,DESCRIPTION,MANUFACTURER,CATEGORY,SUB_CATEGORY,PRODUCT_SIZE) from '/home/olksndr/project/transactions.csv' DELIMITER ',' CSV HEADER;
+
+select * from transactions LEFT JOIN products on transactions.UPC = products.UPC limit 25 ## left join
 
 
 SELECT (store_num, SUM(spend)) from transactions where upc = 1111009477 GROUP BY store_num order by SUM(spend) desc;
